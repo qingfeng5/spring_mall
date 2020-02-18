fe8ec839574210d867a2c19082df305a3ddb5aa6
DESKTOP-0S1FFTP\11365 <wq_qingfeng@163.com>
2020-02-18 12:06:43 +0800

    project init commit

---
+++ src/main/java/com/qingfeng/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.qingfeng.dao;
+
+import com.qingfeng.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
